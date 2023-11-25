a96f8adc8f85572ce938293ca083eeee7693a692
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-11-25 09:03:00 -0300

    fix: title

--- components/Partidas.vue
+++ components/Partidas.vue
@@ -2,7 +2,7 @@
 import _ from 'lodash'
 const supabase = useSupabaseClient()
 const { data: partidasRaw } = await supabase.from('partidas')
-  .select('id, status, data, hora, equipe1 ( id, nome ), equipe2 ( id, nome ), gols ( id, equipe, contra )')
+  .select('id, status, title, data, hora, equipe1 ( id, nome ), equipe2 ( id, nome ), gols ( id, equipe, contra )')
   .order('data', { ascending: true })
   .order('hora', { ascending: true })
 const calendario = _.groupBy(partidasRaw, 'data')
@@ -39,7 +39,7 @@ const getGols = (gols, equipe1, equipe2) => {
       <div v-for="(partidas, data) in calendario" :key="data" class="py-6">
         <h3 class="text-center text-2xl font-bold italic mb-2">
           {{ data.substr(8, 2) }} / {{ data.substr(5, 2) }}
-          {{ partidas[data] && partidas[data].title ? ` - ${partidas[data].title}` : '' }}
+          {{ partidas[0] && partidas[0].title ? ` - ${partidas[0].title}` : '' }}
         </h3>
         <table class="w-full">
           <tr v-for="partida in partidas" :key="partida.id">
