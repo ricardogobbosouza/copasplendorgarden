c18405d5e17ba09a080b8e62046c42fd9aabb30f
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2024-09-27 11:10:01 -0300

    fix: cache foto

--- server/api/jogador/[id].ts
+++ server/api/jogador/[id].ts
@@ -1,15 +1,24 @@
+import type { SupabaseClient } from '@supabase/supabase-js'
 import { serverSupabaseClient } from '#supabase/server'
 
-export default defineCachedEventHandler(async (event) => {
+export default defineEventHandler(async (event) => {
   const supabase = await serverSupabaseClient(event)
+  const id = getRouterParam(event, 'id')
+  const foto = await cachedJogagdorFoto(supabase, id).catch(() => '')
 
+  return foto
+})
+
+export const cachedJogagdorFoto = defineCachedFunction(async (supabase: SupabaseClient, id?: string) => {
   const { data } = await supabase.from('jogadores')
     .select('id, foto')
-    .eq('id', getRouterParam(event, 'id'))
+    .eq('id', id)
     .limit(1)
     .maybeSingle()
   
-  if (data) {
-    return data.foto
-  }
-}, { maxAge: 60 * 60 * 24 })
+  return data.foto
+}, {
+  maxAge: 60 * 60 * 24,
+  name: 'jogagdorFoto',
+  getKey: (id: string) => id
+})
